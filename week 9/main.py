abea4bf5277d64060ae775f0adba973b2de1b5d8
HoBeedzc <lzqpublic@163.com>
2020-11-06 17:11:31 +0800

    week9 sublime merge test

--- week 9/main.py
+++ week 9/main.py
@@ -1,3 +1,79 @@
+import os
+import sys
+import faker
+
+
+class CorpusChristiClockBase():
+    '''
+    '''
+    def __init__(self):
+        pass
+
+
+class CorpusChristiClockCore(CorpusChristiClockBase):
+    '''
+    '''
+    def __init__(self):
+        pass
+
+
+class CCCDecoratorTools:
+    '''
+    '''
+    def __init__(self):
+        pass
+
+    @staticmethod
+    def show_running_time():
+        '''
+        '''
+        pass
+
+    @staticmethod
+    def show_process_rate():
+        '''
+        '''
+        pass
+
+    @staticmethod
+    def show_memory_useage():
+        '''
+        '''
+        pass
+
+    @staticmethod
+    def show_run_info():
+        '''
+        '''
+        pass
+
+    @staticmethod
+    def check_path():
+        '''
+        '''
+        pass
+
+    @staticmethod
+    def play_music_after_running():
+        '''
+        '''
+        pass
+
+
+class CCCProxy(CorpusChristiClockBase):
+    '''
+    '''
+    def __init__(self):
+        pass
+
+
+class CCCTest:
+    '''
+    '''
+    def __init__(self):
+        pass
+
+
 def main():
     pass
 
