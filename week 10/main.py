7d27477034033afbbad2a51c428dd6ee07949a7d
HoBeedzc <lzqpublic@163.com>
2020-11-13 21:07:52 +0800

    week10 on vsc format

--- week 10/main.py
+++ week 10/main.py
@@ -15,13 +15,14 @@ def random_walk(mu, x0, sigma2, N):
         cnt += 1
     return 'Complete'
 
-def creat_param_for_walk(N = 0):
+
+def creat_param_for_walk(N=0):
     '''
     '''
     param_dict = {}
-    param_dict['mu'] = random.uniform(1,100)
-    param_dict['x0'] = random.randint(1,10)
-    param_dict['sigma2'] = random.uniform(1,100)
+    param_dict['mu'] = random.uniform(1, 100)
+    param_dict['x0'] = random.randint(1, 10)
+    param_dict['sigma2'] = random.uniform(1, 100)
     param_dict['N'] = N
     return param_dict
 
@@ -30,7 +31,7 @@ def random_walks(num):
     '''
     '''
     walk_list = []
-    N = random.randint(10,100)
+    N = random.randint(10, 100)
     # make param
     for i in range(num):
         param = creat_param_for_walk(N=N)
@@ -41,14 +42,15 @@ def random_walks(num):
     return walk_zip
 
 
-def plot_random_walk(walk_zip,save_to = r'./img.png'):
+def plot_random_walk(walk_zip, save_to=r'./img.png'):
     '''
     '''
-    x = [i+1 for i in range(len(walk_zip))]
+    x = [i + 1 for i in range(len(walk_zip))]
     for i in range(len(walk_zip[0])):
         y = [j[i] for j in walk_zip]
-        plt.plot(x,y)
+        plt.plot(x, y)
     plt.show()
+
 
 class FaceDataSet:
     '''
