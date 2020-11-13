b9379e6fd44e3d09d99ca3ab172854e8d44c996b
HoBee <lzqpublic@163.com>
2020-11-13 12:45:43 +0000

    week10 on codespace

--- week 10/main.py
+++ week 10/main.py
@@ -15,18 +15,40 @@ def random_walk(mu, x0, sigma2, N):
         cnt += 1
     return 'Complete'
 
+def creat_param_for_walk(N = 0):
+    '''
+    '''
+    param_dict = {}
+    param_dict['mu'] = random.uniform(1,100)
+    param_dict['x0'] = random.randint(1,10)
+    param_dict['sigma2'] = random.uniform(1,100)
+    param_dict['N'] = N
+    return param_dict
+
 
 def random_walks(num):
     '''
     '''
-    pass
+    walk_list = []
+    N = random.randint(10,100)
+    # make param
+    for i in range(num):
+        param = creat_param_for_walk(N=N)
+        print('walk{} params: '.format(i), param)
+        temp = random_walk(**param)
+        walk_list.append(temp)
+    walk_zip = zip(*walk_list)
+    return walk_zip
 
 
-def plot_random_walk():
+def plot_random_walk(walk_zip,save_to = r'./img.png'):
     '''
     '''
-    pass
-
+    x = [i+1 for i in range(len(walk_zip))]
+    for i in range(len(walk_zip[0])):
+        y = [j[i] for j in walk_zip]
+        plt.plot(x,y)
+    plt.show()
 
 class FaceDataSet:
     '''
