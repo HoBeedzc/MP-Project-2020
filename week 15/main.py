fae15b08b55bc5ea1f6aa8748d64fc6f788afca9
HoBee <lzqpublic@163.com>
2020-12-19 14:53:35 +0800

    week15 update on 12.19

--- week 15/main.py
+++ week 15/main.py
@@ -1,11 +1,12 @@
 import asyncio
 from functools import wraps
+import os
+import sys
 
 
 class CONFIG:
     """
     """
-
     @staticmethod
     def yeild_init(func):
         @wraps(func)
@@ -16,49 +17,81 @@ class CONFIG:
 
         return wrapper
 
+
 class Result:
     """
-
     """
-
-    def __init__(self,name,path,line,content):
+    def __init__(self, name, path, line, content):
         self.name = name
         self.path = path
         self.line = line
         self.content = content
         pass
 
-class Search:
-    """
-    """
 
+class Searcher:
+    """
+    """
     def __init__(self):
+        pass
+
+    def search(self,path):
+        """
+        """
+        while True:
+            path = yield  #获取参数
+            g = os.walk(path)
+            for _dir, _, files in g:
+                for file in files:
+                    if file.find('.py') > 0:
+                        file_path = os.path.join(_dir, file)
+                        print('in search, start target')
+                        target.send(file_path)  #传值给别的生成器
+                        print('in search, after the send')
+        pass
+
+
+class Opener:
+    """
+    a parent class for open files
+    """
+    def __init__(self):
+        pass
+
+    def open(self):
         pass
 
 
 class Reader:
     """
-    """
-    pass
-
-
-class Hit:
-    """
-    """
-    pass
-
-class TextSearch(Search):
-    """
-
+    a parent class for read files
     """
     def __init__(self):
-        super().__init__()
         pass
 
-class SearchResult:
+    def read(self):
+        pass
+
+
+class Judger:
     """
     """
-    pass
+    def __init__(self):
+        pass
+
+    def judge(self):
+        pass
+
+
+class Hitter:
+    """
+    a parent class for hit
+    """
+    def __init__(self):
+        pass
+
+    def hit(self):
+        pass
 
 
 class LoaclMiner:
